c1f05e175d4393b659533f22f0bb9b0f98ff1d6b
Alexandros Biratsis <abiratsis@olbico.nl>
2017-11-07 22:29:10 +0100

    First commit

--- NumbersToRoman/Program.cs
+++ NumbersToRoman/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using RomanNumbers.Library;
+using RomanNumbers.Library.Validation.Exceptions;
 
 namespace NumbersToRoman
 {
     class Program
     {
+        // 1 -> I
+        // 2 -> II
+        // 4 -> IV
+        // 3880 -> MMMDCCCLXXX
+        // 322 -> CCCXXII
+        // 21 -> XXI
+        // 999 -> CM XC IX
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num = 0;
+
+            try
+            {
+                var convertor = new ArabicToRomanConvertor(num.ToString());
+                string roman = convertor.Convert();
+
+                Console.WriteLine($"Roman value of {num} is {roman}");
+            }
+            catch (RomanNumberBaseException ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.WriteLine("Press any key to continue!");
+            Console.ReadKey();
         }
     }
 }
